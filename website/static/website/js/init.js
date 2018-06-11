0b70b5b448e5f2d2e42a10119f88c0d9ad0cad7c
Unknown <shobu13@hotmail.fr>
2018-06-11 16:05:06 +0200

    11-06-2018.0
    ajout materialize

---
+++ website/static/website/js/init.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.parallax').parallax();
+    $('.dropdown-trigger').dropdown();
+});
+/*$(document).ready(function() {
+    $(".dropdown-button").dropdown();
+});*/
---
+++ website/static/website/js/scroller.js
@@ -0,0 +1,10 @@
+$('a[href^="#"]').click(function () {
+    var the_id = $(this).attr("href");
+    if (the_id === '#') {
+        return;
+    }
+    $('html, body').animate({
+        scrollTop: $(the_id).offset().top
+    }, 'slow');
+    return false;
+});
