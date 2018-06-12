813a6c629541664ba5ab082b8bec24d9b718b975
Unknown <shobu13@hotmail.fr>
2018-06-12 16:08:54 +0200

    12-06-2018.2
    add CSS t front page and mobile thing

--- vignes/urls.py
+++ vignes/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('paypal', include('paypal.standard.ipn.urls')),
