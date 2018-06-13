9b5cc401fcd9ea2b94aaead43808946de124a83f
Unknown <shobu13@hotmail.fr>
2018-06-13 08:35:43 +0200

    Revert "13-06-2018.0"
    This reverts commit ab04af02bd3178923cf82c9b97b05dafc4ae193f.

--- static/js/categorie_dropdown.js
+++ static/js/categorie_dropdown.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-    cat_dropdown = $('#dropdown1')
+    cat_dropdown_classic = $('#dropdown_classic');
+    cat_dropdown_classic_mobile = $('#dropdown_mobile');
     $.get('/ajax/get_cat', {}, function (data) {
         console.log(data['HTTPRESPONSE']);
         reponse = data['liste_cat'];
         reponse.forEach(function (categorie) {
-        cat_dropdown.append("<li><a href=/magasin/" + categorie[0] + ">" + categorie[1] + "</li>");
+        cat_dropdown_classic.append("<li><a href=/magasin/" + categorie[0] + ">" + categorie[1] + "</li>");
+        cat_dropdown_classic_mobile.append("<li><a href=/magasin/" + categorie[0] + ">" + categorie[1] + "</li>");
         })
     });
 })
