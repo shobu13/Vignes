1114a1cb954c7ea254e4d6628136142a842128ac
Unknown <shobu13@hotmail.fr>
2018-07-06 18:29:40 +0200

    bruh

--- vignes/settings.py
+++ vignes/settings.py
@@ -83,11 +83,8 @@ WSGI_APPLICATION = 'vignes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vignes',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
