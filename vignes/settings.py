307d452ab73571c1f92dd472fb96f69a3ffa7eb0
Unknown <shobu13@hotmail.fr>
2018-07-17 13:39:10 +0200

    17/07/2018
    modification de la page d'acceuil et du modèle card

--- vignes/settings.py
+++ vignes/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'vignes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vignes',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
