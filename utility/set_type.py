28b9600f0745fbe4d7c5c0b399f4c5e59734658c
Unknown <shobu13@hotmail.fr>
2018-07-31 16:45:27 +0200

    maj

--- utility/set_type.py
+++ utility/set_type.py
@@ -5,6 +5,6 @@ def set_type():
     for type in TypesProduit.objects.all():
         print(type)
         for categorie in type.categories.all():
-            for produit in categorie.produit_set.all()
+            for produit in categorie.produit_set.all():
                 produit.type = type
                 produit.save()
