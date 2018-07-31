72e22d5e72e147db0d2fd6378d3aa0516cb26f01
Unknown <shobu13@hotmail.fr>
2018-07-31 16:48:55 +0200

    debug

--- utility/set_type.py
+++ utility/set_type.py
@@ -5,6 +5,8 @@ def set_type():
     for type in TypesProduit.objects.all():
         print(type)
         for categorie in type.categories.all():
+            print("\t{}".format(categorie))
             for produit in categorie.produit_set.all():
+                print("\t\t{}".format(produit))
                 produit.type = type
-                produit.save()
+                # produit.save()
