5542fd3db9d5ca1cc9b6c1d57b3653ff76208947
Unknown <shobu13@hotmail.fr>
2018-08-31 19:51:19 +0200

    fix poid4

--- utility/fix_poid.py
+++ utility/fix_poid.py
@@ -4,6 +4,8 @@ from magasin.models import Produit
 def set_poid():
     try:
         for produit in Produit.objects.all():
+            print(produit.nom)
+            print("\t{}".format(produit.poid))
             produit.poid = produit.poid * 10
             produit.save()
     except BaseException as e:
